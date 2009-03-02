8af67fc0388f8a0085d1b35bc2b54df3b51848ee
Mike Harris <GFunk913@gmail.com>
2009-03-02 13:39:30 -0500

    Only show 2 moves in list

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,6 +1,6 @@
 class MovesController < ApplicationController
   def grid_data
-    @moves = Move.all(:order => "#{params[:sidx]} #{params[:sord]}")
+    @moves = Move.all(:order => "#{params[:sidx]} #{params[:sord]}")[0..1]
 
     respond_to do |format|
       format.xml { render :partial => 'grid_data.xml.builder', :layout => false }
