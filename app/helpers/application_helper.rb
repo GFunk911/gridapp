d583273c84cd29dbf6523e93e7824bcf8ce3a4ba
Mike Harris <GFunk913@gmail.com>
2009-04-07 13:24:27 -0400

    removed render_whole_grid from application's application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,4 @@ module ApplicationHelper
   def pages
     PageManager.get(self).pages
   end
-  def render_whole_grid(table)
-    render_grid(:table => table, :searchField => 'table', :searchString => table, :subtitle => table, :sopt => 'cn')
-  end
 end
