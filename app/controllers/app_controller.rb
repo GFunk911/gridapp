f377ffd6d334aea67b952a72f1a653b2cc038152
Mike Harris <GFunk913@gmail.com>
2009-04-08 14:39:05 -0400

    plumbing tables

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -5,6 +5,7 @@ end
 
 class AppController < ApplicationController
   def show
+    @plumbing = !!params[:plumbing]
     params[:app] = params[:id]
     @app = App.get(params[:id])
     @new_table = NewTable.new(:table => "", :app => @app.app)
--- app/helpers/app_helper.rb
+++ app/helpers/app_helper.rb
@@ -1,2 +1,9 @@
 module AppHelper
+  def tables_to_show
+    if @plumbing
+      @app.tables
+    else
+      @app.tables.reject { |x| %w(columns).include?(x.to_s) }
+    end
+  end
 end
