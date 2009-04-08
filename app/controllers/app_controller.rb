73a29179e80bf44045127d30508ffa2e54e42c52
Mike Harris <GFunk913@gmail.com>
2009-04-08 16:21:43 -0400

    added ability to scrape by specifying xpath, params and row values

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -4,16 +4,22 @@ class NewTable
 end
 
 class AppController < ApplicationController
-  def show
+  def show_setup
+    puts params.inspect
     @plumbing = !!params[:plumbing]
-    params[:app] = params[:id]
-    @app = App.get(params[:id])
+    params[:app] ||= params[:id]
+    @app = App.get(params[:app])
     @new_table = NewTable.new(:table => "", :app => @app.app)
+  end
+  def show
+    show_setup
   end
   def scrape
     app = App.get(params[:app])
     row = app.get_table('scrapes').docs.first
     s = Scraper.new(row)
-    s.run!
+    s.create_rows!
+    show_setup
+    redirect_to :controller => 'app', :action => 'show', :app => params[:app]
   end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,63 @@
+require 'hpricot'
+require 'open-uri'
+require 'ostruct'
+
 class Scraper
-  attr_accessor :code, :target_table, :url, :app
+  attr_accessor :raw_url, :xpath1, :xpath2, :row_code, :app, :param_code
   def initialize(row)
-    @code = row['code']
-    @target_table = row['target_table']
-    @url = row['url']
+    @row_code = row['row_code']
+    @xpath1 = row['xpath1']
+    @xpath2 = row['xpath2']
+    @raw_url = row['url']
     @app = row['app']
+    @param_code = row['param_code']
   end
-  def run!
-    require 'hpricot'
-    require 'open-uri'
-    doc = Hpricot(open(url))
-    res = eval(code)
-    res.each do |h|
-      hash = h.merge(:app => app, :table => target_table)
-      get_app.db.save_doc(hash)
+  def url
+    s = OpenStruct.new
+    eval(param_code).each { |k,v| puts [k,v].inspect; s.send("#{k}=",v) }
+    s.instance_eval("\"#{raw_url}\"")
+  end
+  fattr(:doc) { Hpricot(open(url)) }
+  def to_innerText(arr)
+    if arr.respond_to?(:each)
+      arr.map { |x| to_innerText(x) }
+    else
+      arr.innerText
     end
+  end
+  def result_rows_raw_old
+    [xpath1,xpath2].inject(doc) do |docs,exp| 
+      if docs.is_a?(Array)
+        docs.map { |x| x.search(exp) }
+      else
+        docs.search(exp)
+      end
+    end
+  end
+  def result_rows_raw
+    res = doc.search(xpath1)
+    res = res.map { |x| x.search(xpath2) }
+    res
+  end
+  def result_rows
+    result_rows_raw.reject { |x| x.empty? }.map { |x| to_innerText(x) }
+  end
+  fattr(:row_hashes) do
+    result_rows.map { |x| ScraperRowEval.new(:row_code => row_code, :row => x).row_hash.merge(:app => app) }
+  end
+  def create_rows!
+    puts "Creating #{row_hashes.size} rows from #{url}"
+    row_hashes.each { |h| get_app.db.save_doc(h) }
   end
   def get_app
     App.get(app)
+  end
+end
+
+class ScraperRowEval
+  attr_accessor :row_code, :row
+  include FromHash
+  def row_hash
+    instance_eval(row_code)
   end
 end
