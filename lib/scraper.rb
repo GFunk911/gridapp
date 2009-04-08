a228bc09dce6442d2a5ad2d41e88558bbe68f2c3
Mike Harris <GFunk913@gmail.com>
2009-04-08 18:01:34 -0400

    support parameterized urls in scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,16 +5,34 @@ require 'ostruct'
 class Scraper
   attr_accessor :raw_url, :xpath1, :xpath2, :row_code, :app, :param_code
   def initialize(row)
+    @row = row
+    @param_code = row['param_code']
+  end
+  def params_list
+    res = eval(param_code)
+    res = [res] unless res.is_a?(Array)
+    res
+  end
+  def instances
+    params_list.map { |x| i = ScraperInstance.new(@row); i.params = x; i }
+  end
+  def create_rows!
+    instances.each { |x| x.create_rows! }
+  end
+end
+
+class ScraperInstance
+  attr_accessor :raw_url, :xpath1, :xpath2, :row_code, :app, :params
+  def initialize(row)
     @row_code = row['row_code']
     @xpath1 = row['xpath1']
     @xpath2 = row['xpath2']
     @raw_url = row['url']
     @app = row['app']
-    @param_code = row['param_code']
   end
   def url
     s = OpenStruct.new
-    eval(param_code).each { |k,v| puts [k,v].inspect; s.send("#{k}=",v) }
+    params.each { |k,v| s.send("#{k}=",v) }
     s.instance_eval("\"#{raw_url}\"")
   end
   fattr(:doc) { Hpricot(open(url)) }
@@ -23,15 +41,6 @@ class Scraper
       arr.map { |x| to_innerText(x) }
     else
       arr.innerText
-    end
-  end
-  def result_rows_raw_old
-    [xpath1,xpath2].inject(doc) do |docs,exp| 
-      if docs.is_a?(Array)
-        docs.map { |x| x.search(exp) }
-      else
-        docs.search(exp)
-      end
     end
   end
   def result_rows_raw
@@ -43,7 +52,7 @@ class Scraper
     result_rows_raw.reject { |x| x.empty? }.map { |x| to_innerText(x) }.map { |x| x.is_a?(Array) ? x : [x] }
   end
   fattr(:row_hashes) do
-    result_rows.map { |x| ScraperRowEval.new(:row_code => row_code, :row => x).row_hash.merge(:app => app) }
+    result_rows.map { |x| ScraperRowEval.new(:row_code => row_code, :row => x, :params => params).row_hash.merge(:app => app) }
   end
   def create_rows!
     puts "Creating #{row_hashes.size} rows from #{url}"
@@ -55,8 +64,11 @@ class Scraper
 end
 
 class ScraperRowEval
-  attr_accessor :row_code, :row
+  attr_accessor :row_code, :row, :params
   include FromHash
+  def param
+    params
+  end
   def row_hash
     instance_eval(row_code)
   end
