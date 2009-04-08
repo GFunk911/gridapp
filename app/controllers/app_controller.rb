4641f78f5020fdf07a972d05e65c4809aaf8b38c
Mike Harris <GFunk913@gmail.com>
2009-04-08 14:39:50 -0400

    scrape action

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -10,4 +10,10 @@ class AppController < ApplicationController
     @app = App.get(params[:id])
     @new_table = NewTable.new(:table => "", :app => @app.app)
   end
+  def scrape
+    app = App.get(params[:app])
+    row = app.get_table('scrapes').docs.first
+    s = Scraper.new(row)
+    s.run!
+  end
 end
